e40b12671fc4b505098b0dfe0d9c2000e8504424
George Vorobyev <quaffle97@gmail.com>
2014-03-18 13:15:18 -0400

    added set insert remove

--- 8-LinkedList/MyLinkedList.java
+++ 8-LinkedList/MyLinkedList.java
@@ -9,11 +9,17 @@ public class MyLinkedList{
 		m.add("c");
 		m.add("d");
 		print(m.toString());
-		print(m.get(0));	
-		print(m.get(1));	
-		print(m.get(2));	
-		print(m.get(3));	
-		print(m.get(4));	
+		m.insert(2,"b-c");
+		print(m.toString());
+		m.remove(3);
+		print(m.toString());
+		print(String.valueOf(m.length()));
+		m.remove(3);
+		print(m.toString());
+		print(String.valueOf(m.length()));
+		m.remove(0);
+		print(m.toString());
+		print(String.valueOf(m.length()));
 	}
 	
 	public MyLinkedList(){
@@ -36,6 +42,46 @@ public class MyLinkedList{
 		length++;
 	}
 
+	public void insert(int i, String s){
+		if(i >= length || i < 0) return;
+		Node insert = new Node(s);
+		if(i==0){
+			insert.setNext(start);
+			start = insert;
+			length++;
+			return;
+		}
+		Node temp = start;//temp is before the one to be replaced
+		while(i != 1){
+			temp = temp.next();
+			i--;
+		}
+		if(temp.next() != null)
+			insert.setNext(temp.next());
+		temp.setNext(insert);
+		length++;
+	}
+
+	public void remove(int i){
+		if(i >= length || i < 0) return;
+		if(i == 0){
+			start = start.next();
+			length--;
+			return;
+		}
+		Node temp = start;//predecessor of list(i)
+		while(i!=1){
+			temp = temp.next();
+			i--;
+		}
+		if(temp.next().next() == null)
+			temp.setNext(null);
+		else
+			temp.setNext(temp.next().next());
+		length--;
+	}
+		
+
 	public String get(int i){
 		if(i >= length || i < 0) return null;
 		Node temp = start;
@@ -45,6 +91,18 @@ public class MyLinkedList{
 		}
 		return temp.get();
 	}
+
+	public void set(int i, String s){
+		if(i >= length || i < 0) return;
+		Node temp = start;
+		while(i != 0){
+			temp = temp.next();
+			i--;
+		}
+		temp.set(s);
+	}
+
+	public int length(){return length;}
 
 	public static void print(String s){System.out.println(s);}
 
